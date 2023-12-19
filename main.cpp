7352b82462041aa262c62aa547e1fbab3ec1ccd1
yhudenko <99988012+yhudenko@users.noreply.github.com>
2023-12-19 08:21:10 +0200

    Fix data gain

--- main.cpp
+++ main.cpp
@@ -16,32 +16,22 @@ void handle_receive(const std::error_code& error, std::size_t bytes_transferred)
 
         for (std::size_t i = 0; i < bytes_transferred; ++i) {
             if (mavlink_parse_char(MAVLINK_COMM_0, buffer[i], &msg, &status)) {
-                /*for(auto c : buffer)
-                {
-                std::cout << c;
-                }
-                std::cout << std::endl;*/
                 switch (msg.msgid) {
                     case MAVLINK_MSG_ID_HEARTBEAT:
                         mavlink_heartbeat_t heartbeat;
                         mavlink_msg_heartbeat_decode(&msg, &heartbeat);
 
-                        // Extract information from the heartbeat message
                         std::cout << "Received Heartbeat: System ID - " << (int)msg.sysid
                             << ", Component ID - " << (int)msg.compid << std::endl;
                         break;
                     case MAVLINK_MSG_ID_SCALED_PRESSURE:
-                        mavlink_pressure_t pressure;
-                        mavlink_msg_pressure_decode(msg, &pressure);
-                        std::cout << "Received Pressure Message: " << pressure.pressure << " Pascal" << std::endl;
-                        break;
-                    case MAVLINK_MSG_ID_SCALED_IMU:
-                        mavlink_temperature_t temperature;
-                        mavlink_msg_temperature_decode(msg, &temperature);
-                        std::cout << "Received Temperature Message: " << temperature.temperature << " degrees Celsius" << std::endl;
-                        break;
-                    default:
-                        std::cout << 'Other'<< std::endl;
+                        mavlink_scaled_pressure_t scaled_pressure;
+                        mavlink_msg_scaled_pressure_decode(&msg, &scaled_pressure);
+
+                        float temperature = scaled_pressure.temperature;
+                        uint32_t pressure = scaled_pressure.press_abs;
+
+                        std::cout << "Temperature: " << temperature << " C, Pressure: " << pressure << " Pa" << std::endl;
                 }
             }
         }
@@ -52,6 +42,7 @@ void handle_receive(const std::error_code& error, std::size_t bytes_transferred)
 }
 
 int main() {
+    std::cout << "Received message" <<std::endl;
     socket_other.async_receive_from(boost::asio::buffer(buffer), sender_endpoint, handle_receive);
     io_service.run();
 
